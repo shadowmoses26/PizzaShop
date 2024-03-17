21b5e94227941284bccfca24ecc7b3c1ec76beb2
shadowmoses26 <dracula094@mail.ru>
2024-03-17 14:50:06 +0300

    Keep x variable in localStorage

--- public/app.js
+++ public/app.js
@@ -1,6 +1,9 @@
 function something()
 {
-    var x = window.localStorage.getItem('aaaa');
-    window.localStorage.setItem('aaaa',555);
+    var x = window.localStorage.getItem('bbb');
+
+    x = x * 1 + 1;
+
+    window.localStorage.setItem('bbb',555);
     alert(x);
 }
