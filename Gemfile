aac806af36df4ea7de7769930743f94686cf00a5
shadowmoses26 <dracula094@mail.ru>
2024-03-17 15:05:26 +0300

    Add rakefile, update gemfile and model

--- Gemfile
+++ Gemfile
@@ -1,3 +1,9 @@
 source "https://rubygems.org"
 
 gem "sinatra"
+gem "pg"
+gem "activerecord"
+gem "sinatra-activerecord"
+gem "sinatra-contrib"
+gem "rake"
+gem "rackup"
--- app.rb
+++ app.rb
@@ -1,7 +1,20 @@
 #encoding: utf-8
 require 'rubygems'
 require 'sinatra'
+require 'sinatra/base'
 require 'sinatra/reloader'
+require 'sinatra/activerecord'
+require 'rackup'
+
+
+configure :development do
+	#set :port, 5433
+	set :database,{adapter: 'postgresql',  encoding: 'unicode', database: 'pizzaShop', pool: 2, username: 'postgres', password: 'root', port: '5433'}
+end
+
+class Product < ActiveRecord::Base
+
+end
 
 get '/' do
 	erb :index
