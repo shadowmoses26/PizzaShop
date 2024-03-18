ee1bb57f43833423b80a12b43b0f1bdba744e45e
shadowmoses26 <dracula094@mail.ru>
2024-03-18 15:13:01 +0300

    Add cart_get_number_of_items function

--- public/app.js
+++ public/app.js
@@ -1,18 +1,34 @@
-function something()
-{
+function something() {
     var x = window.localStorage.getItem('bbb');
 
     x = x * 1 + 1;
 
-    window.localStorage.setItem('bbb',555);
+    window.localStorage.setItem('bbb', 555);
     alert(x);
 }
 
 
-function add_to_cart(id)
-{
+function add_to_cart(id) {
     var key = 'product_' + id
     var x = window.localStorage.getItem(key);
     x = x * 1 + 1;
     window.localStorage.setItem(key, x);
+
+    alert('Items in your cart: ' + cart_get_number_of_items() );
+}
+
+function cart_get_number_of_items() {
+    var cnt = 0;
+
+    for (var i = 0; i < localStorage.length; i++) {
+
+        var key = window.localStorage.key(i);
+
+        var value = window.localStorage.getItem(key);
+
+        if (key.indexOf('product_') == 0) {
+            cnt = cnt + value * 1;
+        }
+    }
+    return cnt;
 }
